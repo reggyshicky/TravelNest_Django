19937b8480eee658ce64cd4c4d1b3e5a753a2d51
reggyshicky <reginahshikanda@gmail.com>
2023-09-09 19:13:13 +0300

    created my first model

--- TravelNest/accounts/models.py
+++ TravelNest/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    destination = models.CharField(max_length=150)
+    arrivalTime = models.DateTimeField(auto_now_add=True)
+    departureTime = models.DateTimeField(auto_now_add=True)
+    origin = models.CharField(max_length=150)
+    
+    class Meta:
+        abstract=True
+        
+class Flight(BaseModel):
+    
+    
+    def __str__():
+        return ""
+    
+    
