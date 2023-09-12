77df3d9c0054728e7d399f7d0710823b17326b83
reggyshicky <reginahshikanda@gmail.com>
2023-09-12 19:05:46 +0300

    view to display flights available, departure time, created Airport, flight and Train models

--- TravelNest/accounts/models.py
+++ TravelNest/accounts/models.py
@@ -1,19 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=50)
+    airportCode = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name} {self.airportCode}"
+    
+FLIGHT_STATUS =[
+    ("Cancelled", "CANCELLED"),
+    ("Delayed", "DELAYED"),
+    ("On Time", "ON_TIME")
+]   
 class BaseModel(models.Model):
-    destination = models.CharField(max_length=150)
+    
     arrivalTime = models.DateTimeField(auto_now_add=True)
     departureTime = models.DateTimeField(auto_now_add=True)
-    origin = models.CharField(max_length=150)
-    
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places =2)
+    status = models.CharField(choices=FLIGHT_STATUS, max_length=10, blank=True, null=True)
     class Meta:
-        abstract=True
-        
+        abstract=True   # no class called BaseModel will be creating in the database
+
+    
 class Flight(BaseModel):
+    pointOfDeparture = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="flight_time")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="flight_destination")
+     
+    class Meta:
+         ordering = ("pointOfDeparture",)
+         
+    def __str__(self):
+        return f"{self.pointOfDeparture}, {self.destination}, {self.departureTime}, {self.arrivalTime}"
     
-    
-    def __str__():
-        return ""
-    
-    
+class Train(BaseModel):
+    pointOfDeparture = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="Train_time")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="Train_destination")
+    def __str__(self):
+        return f"{self.duration}, {self.price} {self.status}"
--- TravelNest/accounts/views.py
+++ TravelNest/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Flight, Train, Airport
 
 # Create your views here.
 def home(request):
-    return render(request, "accounts/index.html")
+    
+    flights = Flight.objects.all()
+    context = {
+        "flights" : flights,
+        
+    }
+    return render(request, "accounts/index.html", context)
